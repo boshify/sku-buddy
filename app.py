d9cb731a0965538655449e42c2b7ae8aef566d78
boshify <140970186+boshify@users.noreply.github.com>
2024-10-10 22:15:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -131,11 +131,7 @@ if 'master_df' in st.session_state and 'supplier_df' in st.session_state:
                     skus_updated += 1
 
             # Find products from supplier that are not in master
-            if match_key_supplier in supplier_df.columns:
-                unmatched_df = supplier_df[~supplier_df[match_key_supplier].isin(matched_df[match_key_supplier])]
-            else:
-                unmatched_df = supplier_df[~supplier_df[match_key_supplier].isin(master_df[match_key_master])]
-            
+            unmatched_df = supplier_df[~supplier_df[match_key_supplier].isin(matched_df[match_key_supplier])]
             products_not_in_master = len(unmatched_df)
 
             # Store results in session state to prevent reset after download
